75184ce11acd8a17d5f7969778a3828f38be5df1
Eugene Ong <eugeneong2021@gmail.com>
2021-03-23 07:19:45 +0800

    Initial Commit

---
+++ Data/DataContext.cs
@@ -0,0 +1,15 @@
+using Data.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
